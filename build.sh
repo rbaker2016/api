902bf36c8d6c2703412ae9333f21579678f27af4
pouya <pouya@applariat.com>
2017-08-09 14:59:27 -0700

    just using build artifact with build args

--- build.sh
+++ build.sh
@@ -1,31 +1,25 @@
 #! /bin/bash
-# appLariats generic Python build script
-# Requirements - A valid requirements.txt file exists within the code_dir
-# Copies the requirements.txt file from the code_dir and copies it into /usr/src/app/
 
 #Log everything in /code/build.log
 logfile=/tmp/build.log
 exec > $logfile 2>&1
 set -x
 
-TAG=${TAG}
+#Mapping build ARGs
+APL_API_TAG={$APL_API_TAG}
 APL_COMMON_TAG=${APL_COMMON_TAG}
 BB_API_KEY=${BB_API_KEY}
 
-echo "print TAG value:"
-echo $TAG
-echo "print BB_API_KEY value:"
-echo $BB_API_KEY
+#Installing some tools
+apt update && apt install -y wget bsdtar openssl libc-dev gcc
+
+#Pulling apl-api repo
+wget https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-api/get/$APL_API_TAG.zip
 
 mkdir -p /usr/src/app
-cp -rf /code/* /usr/src/app/
+bsdtar -xf $APL_API_TAG.zip -s'|[^/]*/||' -C /usr/src/app
 cd /usr/src/app/
 
-#pulling down and installing code
-apt update \
-    && apt install -y openssl libc-dev gcc \
-	&& pip install . \
-    && cp -rf /code/* . && pip install --upgrade https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-common/get/${APL_COMMON_TAG}.zip
-
-#cleaning after ourselves
-rm -rf /code
+#Pulling down and installing apl-common code
+pip install . \
+    && pip install --upgrade https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-common/get/${APL_COMMON_TAG}.zip
