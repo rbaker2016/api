796557aeed7eac770a7f98b9d0f9fd5686fee7d4
pouya <pouya@applariat.com>
2017-08-09 16:21:20 -0700

    debugging1

--- build.sh
+++ build.sh
@@ -13,13 +13,13 @@ BB_API_KEY=${BB_API_KEY}
 #Installing some tools
 apt update && apt install -y wget bsdtar openssl libc-dev gcc
 
-#Pulling apl-api repo
-wget https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-api/get/$APL_API_TAG.zip
-
-mkdir -p /usr/src/app
-bsdtar -xf $APL_API_TAG.zip -s'|[^/]*/||' -C /usr/src/app
-cd /usr/src/app/
-
-#Pulling down and installing apl-common code
-pip install . \
-    && pip install --upgrade https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-common/get/${APL_COMMON_TAG}.zip
+##Pulling apl-api repo
+#wget https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-api/get/$APL_API_TAG.zip
+#
+#mkdir -p /usr/src/app
+#bsdtar -xf $APL_API_TAG.zip -s'|[^/]*/||' -C /usr/src/app
+#cd /usr/src/app/
+#
+##Pulling down and installing apl-common code
+#pip install . \
+#    && pip install --upgrade https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-common/get/${APL_COMMON_TAG}.zip
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ cat /tmp/build.log
 APP="Application"
 
 echo "Starting API..."
-exec python - <<-EOF
-from apl_api import api
-ws_app = api.${APP}()
-ws_app.run(8080)
-EOF
+#exec python - <<-EOF
+#from apl_api import api
+#ws_app = api.${APP}()
+#ws_app.run(8080)
+#EOF
