665b5d1347fdca5349f1acf4b1753e7bb0ad45dc
pouya <pouya@applariat.com>
2017-08-09 16:38:16 -0700

    debugging4

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ APL_API_TAG=${APL_API_TAG}
 APL_COMMON_TAG=${APL_COMMON_TAG}
 BB_API_KEY=${BB_API_KEY}
 
+echo "BB_API_KEY= $BB_API_KEY"
 
 #Installing some tools
 apt update && apt install -y wget bsdtar openssl libc-dev gcc
@@ -20,7 +21,8 @@ wget https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-api/get/$APL_API_
 mkdir -p /usr/src/app
 bsdtar -xf $APL_API_TAG.zip -s'|[^/]*/||' -C /usr/src/app
 cd /usr/src/app/
+ls -alh
 
 #Pulling down and installing apl-common code
-pip install . \
-    && pip install --upgrade https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-common/get/${APL_COMMON_TAG}.zip
+#pip install . \
+#    && pip install --upgrade https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-common/get/${APL_COMMON_TAG}.zip
