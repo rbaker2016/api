a0de9cddf186f21d2bdd7c32fa362627c0f28da2
pouya <pouya@applariat.com>
2017-10-30 13:58:31 -0700

    added support for new build var APL_API_ADMIN_TAG

--- build.sh
+++ build.sh
@@ -8,22 +8,40 @@
 #Mapping build ARGs
 APL_API_TAG=${APL_API_TAG}
 APL_COMMON_TAG=${APL_COMMON_TAG}
+APL_API_ADMIN_TAG=${APL_API_ADMIN_TAG}
 BB_API_KEY=${BB_API_KEY}
 
-echo "BB_API_KEY= $BB_API_KEY"
+
+#echo "BB_API_KEY= $BB_API_KEY"
 echo "APL_COMMON_TAG= $APL_COMMON_TAG"
 echo "APL_API_TAG= $APL_API_TAG"
+echo "APL_API_ADMIN_TAG=" $APL_API_ADMIN_TAG
 
 #Installing some tools
 apt update && apt install -y wget bsdtar openssl libc-dev gcc
 
-#Pulling apl-api repo
-wget https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-api/get/$APL_API_TAG.zip
+#This repo is used for building api and api-admin, based on build vars different code base will be pulled down
 
-mkdir -p /usr/src/app
-bsdtar -xf $APL_API_TAG.zip -s'|[^/]*/||' -C /usr/src/app
-cd /usr/src/app/
-ls -alh
+#Pulling apl-api repo
+if [ ! -z $APL_API_TAG ]
+then
+  wget https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-api/get/$APL_API_TAG.zip
+  mkdir -p /usr/src/app
+  bsdtar -xf $APL_API_TAG.zip -s'|[^/]*/||' -C /usr/src/app
+  cd /usr/src/app/
+  ls -alh
+fi
+
+#Pulling apl-api-admin repo
+if [ ! -z $APL_API_ADMIN_TAG ]
+then
+  wget https://applariat:$BB_API_KEY@bitbucket.org/applariat/apl-admin-api/get/$APL_API_ADMIN_TAG.zip
+  mkdir -p /usr/src/app
+  bsdtar -xf $APL_API_ADMIN_TAG.zip -s'|[^/]*/||' -C /usr/src/app
+  cd /usr/src/app/
+  ls -alh
+fi
+
 
 Pulling down and installing apl-common code
 pip install . \
